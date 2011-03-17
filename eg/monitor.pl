dfb61cab120655ff5b708583de33c307462e15da
akiym <akiyama@8ant.org>
2011-03-18 02:32:10 +0900

    `perl eg/monitor.pl` if you want to monitor notify

---
+++ eg/monitor.pl
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use File::Spec;                                                                 use File::Basename;
+use lib File::Spec->catdir(dirname(__FILE__), '..', 'lib');                     use Net::DBus::Skype::Lite;
+use Net::DBus::Reactor;
+
+my $skype = Net::DBus::Skype::Lite->new;
+
+my $reactor = Net::DBus::Reactor->main();                                       $reactor->run();
