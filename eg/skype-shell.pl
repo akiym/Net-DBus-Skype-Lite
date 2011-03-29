d1421abd9425da3fa654f252015209b25dfce606
akiym <akiyama@8ant.org>
2011-03-30 02:24:36 +0900

    more easy

--- eg/skype-shell.pl
+++ eg/skype-shell.pl
@@ -5,14 +5,14 @@ use File::Basename;
 use lib File::Spec->catdir(dirname(__FILE__), '..', 'lib');
 use Net::DBus::Skype::Lite;
 use Term::ReadLine;
+use Data::Dumper;
 
 my $skype = Net::DBus::Skype::Lite->new();
 
 my $term = Term::ReadLine->new('Skype shell');
 while (defined(my $cmd = $term->readline('skype> '))) {
-    if ($cmd =~ s/\((.*)\)$//) {
-        my @args = split ', ', ($1 || '');
-        print $skype->$cmd(@args), "\n";
+    if ($cmd =~ /\(.*\)$/) {
+        eval "print Dumper(\$skype->$cmd)";
     } else {
         print $skype->api($cmd), "\n";
     }
